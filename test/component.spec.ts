9e9f15d155e871a443be2c9360e0b50c2703c490
Fabian <faboweber@gmail.com>
2017-02-06 23:29:30 +0100

    added more comments

--- test/component.spec.ts
+++ test/component.spec.ts
@@ -6,8 +6,10 @@ import { mockStore } from './mockStore';
 
 describe('CleverComponent', () => {
 	it('should show clicks', () => {
+		// to test components we just manipulate the input streams
 		let store = mockStore({clicks: { clicks: 5 }});
 		let component$ = CleverComponent({ sinks: { store }});
+		// then we render the result of the hyperscript stream and check the dom result
 		let elem = create(component$(), null);
 		expect(elem.innerText).to.contain('5');
 	});
