dc93c5ffc868e171ae0ef685d78ea62909b26f7b
Fabian <faboweber@gmail.com>
2017-02-28 21:01:43 +0100

    working for lists

--- src/utils/streamy-dom.js
+++ src/utils/streamy-dom.js
@@ -10,50 +10,59 @@ export function createElement(tagName, properties$, children$Arr) {
     return elem;
 }
 
+// IDEA add request animation frame and only update when animating
 function manageChildren(parentElem, children$Arr) {
 	let childElems = [];
 	children$Arr.map((child$, index) => {
+		// TODO if child is array calc positions + refactor
 		child$.map(child => {
+			let rightNeighbor = childElems.slice(index).find(elem => elem !== null);
+			rightNeighbor = Array.isArray(rightNeighbor) ? rightNeighbor[0] : rightNeighbor;
+			let relativePos = rightNeighbor 
+				? Array.prototype.indexOf.call(parentElem.childNodes, rightNeighbor) - 1
+				: parentElem.childNodes.length;
 			// streams can return arrays of children
 			if (Array.isArray(child)) {
-				child.forEach(_child_ => addOrUpdateChild(_child_, childElems));
+				childElems[index] = childElems[index] || [];
+				var clonedParent = parentElem.cloneNode();
+				child.forEach((_child_, _index_) => {
+					childElems[index][_index_] = addOrUpdateChild(_child_, relativePos + _index_, childElems[index][_index_], clonedParent, rightNeighbor);
+				});
+				// if the parent is already attached to the dom we replace it
+				// this is faster then manipulating it often
+				if (!parentElem.parentNode) {
+					return clonedParent;
+				} else {
+					parentElem.parentNode.replaceChild(clonedParent, parentElem);
+				}
 			} else {
-				addOrUpdateChild(child, childElems);
+				childElems[index] = addOrUpdateChild(child, relativePos, childElems[index], parentElem, rightNeighbor);
 			}
 		});
 	});
 }
 
-function addOrUpdateChild(child, childElems) {
+function addOrUpdateChild(child, relativePos, savedElem, parentElem, rightNeighbor) {
 	// if the child was there but got removed
 	// we need to remove the elem
-	if (childElems[index] && child === null) {
+	if (savedElem && child === null) {
 		parentElem.removeChild(childElems[index]);
-		childElems[index] = null;
+		return null;
 	}
-	if (child == null) return;
+	if (child == null) return null;
 
 	if (typeof child === "string" || typeof child === "number") {
 		child = document.createTextNode(child);
 	}
-	// maybe some elements are missing
-	// so we get the relativePos by not counting the missing ones before
-	let relativePos = childElems.slice(0, index)
-		.reduce((pos, elem) => {
-			return pos + elem ? 1 : 0;
-		}, 0);
 	// if the element is not yet drawn
-	if (!childElems[index]) {
-		if (relativePos === 0) {
-			parentElem.appendChild(child);
-		} else {
-			insertAtPosition(child, parentElem, relativePos);
-		}
+	if (!savedElem) {
+		// if rightNeighbor is null it gets appended
+		parentElem.insertBefore(child, rightNeighbor);
 	} else {
 		// if the element is already there then replace it
-		parentElem.replaceChild(child, childElems[index]);
+		parentElem.replaceChild(child, savedElem);
 	}
-	childElems[index] = child;
+	return child;
 }
 
 function manageProperties(elem, properties$) {
