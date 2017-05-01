b1a95417f035db20085463a51d839d034b630c75
Fabian <faboweber@gmail.com>
2017-05-01 22:39:44 +0200

    fixed tests + build

--- test/demo_component.spec.js
+++ test/demo_component.spec.js
@@ -1,51 +1,42 @@
 import { h, stream, list, UPDATE_DONE } from '../src';
-import { CleverComponent, DumbComponent, SuperDumbComponent, ListComponent } from '../demo/demo_component.jsx';
-import { SUBTRACKED } from '../demo/reducers/clicks.js';
 import assert from 'assert';
-import { mockStore } from './helpers/mockStore';
-
-// ATTENTION
-// the rendering of children is sometimes deferred to an animationframe (if there are to many changes)
-// in that case we have to wait till the children are rendered
-// checkout the list example for this case
 
 describe('Components', () => {
-	it('SuperDumpComponent should show', () => {
-		let element = <SuperDumbComponent />;
+	it('should show a component', () => {
+		let element = <p>HELLO WORLD</p>;
 		assert.equal(element.outerHTML, '<p>HELLO WORLD</p>');
 	});
 
-	it('CleverComponent should perform inner operation and show result', () => {
-		// to test components dependent on state we just manipulate the input streams
-		// for the store we use a mocking object
-		let store = mockStore({ clicks: { clicks: 3 }});
-		// this component calculates the clicks * 2 inside and shows the result
-		let component = <CleverComponent store= {store} />;
+	it('should react to inputs', () => {
+		let clicks$ = stream(3);
+		let DoubleClicks = ({clicks$}) =>
+			<p>Clicks times 2: {clicks$.map(clicks => 2*clicks)}</p>;
+		let component = <DoubleClicks clicks$={clicks$} />;
 		assert.equal(component.outerHTML, '<p>Clicks times 2: 6</p>');
 	});
 
 	it('CleverComponent should update on store update', () => {
-		// to test that components react to their inputs we just manipulate the input streams
-		let store = mockStore({ clicks: { clicks: 3 }});
-		let component = <CleverComponent store= {store} />;
+		let clicks$ = stream(3);
+		let DoubleClicks = ({clicks$}) =>
+			<p>Clicks times 2: {clicks$.map(clicks => 2*clicks)}</p>;
+		let component = <DoubleClicks clicks$={clicks$} />;
 		assert.equal(component.outerHTML, '<p>Clicks times 2: 6</p>');
-		store.state$({ clicks: { clicks: 6 }});
+		clicks$(6);
 		assert.equal(component.outerHTML, '<p>Clicks times 2: 12</p>');
 	});
 
-	it('should react to attached events', (done) => {
-		let store = mockStore({});
+	it('should react to attached events', () => {
+		let DumbComponent = ({clicks$}) => 
+			<div>
+				<button onclick={clicks$(clicks$() + 1)}>Click to emit event</button>
+			</div>;
+		let clicks$ = stream(0);
 		// this component fires a action on the store when clicked
-		let element = <DumbComponent store={store} />;
+		let element = <DumbComponent clicks$={clicks$} />;
 		// perform the actions on the element
 		element.querySelector('button').click();
 
-		// we can listen on the action$ to test this happening
-		store.action$.map(action => {
-			if (action == null) return;
-			assert.equal(action.type, SUBTRACKED);
-			done();
-		});
+		assert.equal(clicks$(), 1);
 	});
 
 	it('should render a list of changes in an animationframe', (done) => {
