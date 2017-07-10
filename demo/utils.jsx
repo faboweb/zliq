cc50ef6b0d7d9aeb8c6c64489a2a9f08dc0663df
Fabian Weber <frznhope@gmail.com>
2017-07-10 15:43:19 +0200

    defering prism init for better start performance

--- demo/utils.jsx
+++ demo/utils.jsx
@@ -1,20 +1,23 @@
 import Prism from 'prismjs';
 import 'prismjs/components/prism-jsx.js';
 import 'prismjs/themes/prism-solarizedlight.css';
-import { h } from '../src';
+import { h, stream } from '../src';
 
 export const Markup = (props, children$) => {
     // children are always arrays of arrays to allow children that stream arrays
-    let code$ = children$.map(children => {
-        let code = children[0];
-        let strippedMarginCode = code
-            .split('\n')
-            .filter(line => line.trim() !== '')
-            .map(line => line.trim().substr(1))
-            .join('\n');
-        let html = Prism.highlight(strippedMarginCode, Prism.languages.jsx);
-        return html;
-    })
+    let code$ = stream('');
+    setTimeout(() => {
+        children$.map(children => {
+            let code = children[0];
+            let strippedMarginCode = code
+                .split('\n')
+                .filter(line => line.trim() !== '')
+                .map(line => line.trim().substr(1))
+                .join('\n');
+            let html = Prism.highlight(strippedMarginCode, Prism.languages.jsx);
+            return html;
+        }).map(code$);
+    }, 10);
     return <pre class="language-jsx">
         <code class="language-jsx" innerHTML={code$} />
     </pre>;
