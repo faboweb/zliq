52b2d12e552ae1ccaac393b9628f332ead5c971a
Fabian Weber <frznhope@gmail.com>
2017-07-13 13:51:22 +0200

    removing old import

--- src/utils/index.js
+++ src/utils/index.js
@@ -3,5 +3,3 @@ export * from './streamy-dom';
 export * from './streamy-hyperscript';
 export * from './streamy-helpers';
 export * from './router';
-// under construction
-// export * from './lazy-list';
