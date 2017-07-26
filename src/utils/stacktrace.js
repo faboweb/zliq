c615a0564b4e4600879ef00d3f9d9b88c065e9af
Fabian Weber <frznhope@gmail.com>
2017-07-26 10:52:45 +0200

    better formating

--- src/utils/stacktrace.js
+++ src/utils/stacktrace.js
@@ -3,28 +3,27 @@ import StackTrace from 'stacktrace-js';
 
 /*
 * ATTENTION: Enable sourcemaps in Chrome!!
+*
 */
 export function shrink_stacktrace() {
     window.onerror = function(msg, file, line, col, error) {
         let stack = error.stack.split('\n');
         // callback is called with an Array[StackFrame]
-        StackTrace.fromError(error).then(frames => callback(frames, stack)).catch(errback);
+        StackTrace.fromError(error).then(frames => callback(frames, stack, msg)).catch(errback);
 
         return true;
     };
 
-    function callback(mappedFrames, nativeStack) {
+    function callback(mappedFrames, nativeStack, msg) {
         let x = 0;
         let filteredFrames = mappedFrames
         .filter((stackLine, i) => mappedFrames[i] && mappedFrames[i].fileName.startsWith(`webpack://`))
-        .map(frame => {
-            frame.fileName = frame.fileName.replace(`webpack://`, `webpack:///.`);
-            return frame;
-        })
-        // remove error message
-        filteredFrames.splice(0,1);
+        .map(({functionName, columnNumber, fileName, lineNumber}) => {
+            let chromeFileDirection = fileName.replace(`webpack://`, `webpack:///.`);
+            return `\t${(functionName !== undefined ? functionName  : '(anonymous)')} \t\tat ${chromeFileDirection}:${lineNumber}:${columnNumber}`;
+        });
 
-        let message = ['ZLIQ encountered an error:'].concat(filteredFrames).join('\n');
+        let message = ['(ZLIQ) ' + msg].concat(filteredFrames).join('\n');
         console.error(message);
     }
 
