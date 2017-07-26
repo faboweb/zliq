344178898c0a5d46ec085be791c42ad15ef86689
Fabian Weber <frznhope@gmail.com>
2017-07-26 10:35:03 +0200

    linking to correct sourcemaps works

--- src/utils/stacktrace.js
+++ src/utils/stacktrace.js
@@ -6,19 +6,29 @@ import StackTrace from 'stacktrace-js';
 */
 export function shrink_stacktrace() {
     window.onerror = function(msg, file, line, col, error) {
+        let stack = error.stack.split('\n');
         // callback is called with an Array[StackFrame]
-        StackTrace.fromError(error).then(callback).catch(errback);
+        StackTrace.fromError(error).then(frames => callback(frames, stack)).catch(errback);
 
         return true;
     };
 
-    function callback (frames) {
-        // hide things that are not code
-        let filtered = frames.filter(({fileName}) => fileName.startsWith(`webpack://`));
-        let links = filtered.map(({functionName, columnNumber, fileName, lineNumber}) => `\t${functionName !== undefined ? functionName + ' ' : ''}\tat ${fileName}:${lineNumber}:${columnNumber}`)
-        let message = ['ZLIQ encountered an error:'].concat(links).join('\n');
+    function callback(mappedFrames, nativeStack) {
+        let x = 0;
+        let filteredFrames = mappedFrames
+        .filter((stackLine, i) => mappedFrames[i] && mappedFrames[i].fileName.startsWith(`webpack://`))
+        .map(frame => {
+            frame.fileName = frame.fileName.replace(`webpack://`, `webpack:///.`);
+            return frame;
+        })
+        // remove error message
+        filteredFrames.splice(0,1);
+
+        let message = ['ZLIQ encountered an error:'].concat(filteredFrames).join('\n');
         console.error(message);
     }
 
-    function errback () {};
+    function errback (err) {
+        console.error(err);
+    };
 }
