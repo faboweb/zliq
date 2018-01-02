b4929bafb1c9eaa835366ddd586734d0517fcff4
Fabian <frznhope@gmail.com>
2018-01-03 00:54:23 +0100

    fixed removing demo by accident

--- demo/bundle.js
+++ demo/bundle.js
@@ -5899,26 +5899,22 @@ var errorHandler = (0, _zliqStacktrace.shrinkStacktrace)(/(src\/utils|bootstrap|
 var router$ = (0, _zliqRouter.initRouter)(); // main render function for the application
 // render provided hyperscript into a parent element
 // zliq passes around HTMLElement elements so you can decide what to do with them
-var trigger$ = (0, _src.stream)(true);var cycle = { mounted: function mounted() {}, created: function created() {}, removed: function removed() {} };var component = (0, _src.h)('div', { id: 'test', cycle: cycle }, []);
+var app = (0, _src.h)('div', null, [(0, _src.h)(_header.Header, null, []), (0, _src.h)('div', { 'class': 'container' }, [(0, _src.h)('a', { href: 'https://github.com/faboweb/zliq' }, [(0, _src.h)('img', { style: 'position: absolute; top: 0; right: 0; border: 0;', src: 'https://camo.githubusercontent.com/38ef81f8aca64bb9a64448d0d70f1308ef5341ab/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f6461726b626c75655f3132313632312e706e67', alt: 'Fork me on GitHub', 'data-canonical-src': 'https://s3.amazonaws.com/github/ribbons/forkme_right_darkblue_121621.png' }, [])]), (0, _src.h)(_infos.Infos, null, []), (0, _src.h)('div', { 'class': 'section' }, [(0, _src.h)(_subheader.Subheader, { title: 'Motivation', subtitle: 'Why yet another web framework?', id: 'motivation' }, []),
 
-var app = (0, _src.h)('div', null, [
+			(0, _src.h)('div', { 'class': 'row' }, [
+				(0, _src.h)('p', null, ['Modern web frameworks got really big (React + Redux 139Kb and Angular 2 + Rx 766Kb, ', (0, _src.h)('a', { href: 'https://gist.github.com/Restuta/cda69e50a853aa64912d' }, ['src']), '). As a developer I came into the (un)pleasant situation to teach people how these work. But I couldn\'t really say, as I haven\'t actually understood each line of code in these beasts. But not only that, they also have a lot of structures I as a developer have to learn to get where I want to go. It feels like learning programming again just to be able to render some data.']),
+				(0, _src.h)('p', null, ['ZLIQ tries to be something simple. Something that reads in an evening. But that is still so powerful you can just go and display complex UIs with it. Something that feels more JS less Java.']),
+				(0, _src.h)('p', null, ['Still ZLIQ doesn\'t try to be the next React or Angular! ZLIQ has a decent render speed even up to several hundred simultaneous updates but it\'s not as fast as ', (0, _src.h)('a', { href: '(https://preactjs.com/' }, ['Preact']), '. And it on purpose does not solve every problem you will ever have. ZLIQ is a tool to help you create the solution that fits your need.'])])]),
 
-	(0, _src.if$)(trigger$, component)]);
+
+		(0, _src.h)(_tutorial.Tutorial, { router$: router$ }, []),
+		(0, _src.h)(_playground.Playground, null, [])])]);
 
 
 (0, _src.render)(app, document.querySelector('#app'), {
 	config: {
 		value: 'abc',
 		url: 'www' } });
-
-
-setTimeout(function () {
-	trigger$(false);
-}, 1000);
-
-setTimeout(function () {
-	trigger$(true);
-}, 5000);
 
 /***/ }),
 /* 19 */
