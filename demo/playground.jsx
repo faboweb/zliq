bf75bc45eebb8d410eebcfda8edfbc37d26f2224
Fabian Weber <frznhope@gmail.com>
2018-11-18 22:45:10 +0100

    switched to codepen

--- demo/playground.jsx
+++ demo/playground.jsx
@@ -9,7 +9,8 @@ export const Playground = () => zx`
       subtitle: "Fork and get your hands dirty"
     })}
     <div isolated>
-      <script async src="//jsfiddle.net/582wayrz/embed/js,result/" />
+      <p *data-height="265" *data-theme-id="light" *data-slug-hash="XyeNQv" *data-default-tab="js,result" *data-user="faboweb" data-pen-title="ZLIQ Playground" class="codepen">See the Pen <a href="https://codepen.io/faboweb/pen/XyeNQv/">ZLIQ Playground</a> by Fabian Weber (<a href="https://codepen.io/faboweb">@faboweb</a>) on <a href="https://codepen.io">CodePen</a>.</p>
+      <script async src="https://static.codepen.io/assets/embed/ei.js"></script>
     </div>
   </div>
 `;
